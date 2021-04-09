7a148c442aa61646122ec88e65a3b4d5d5b86573
RickDiesel17 <s.soni87@yahoo.com>
2021-04-09 22:59:36 +0530

    Major Update. Second login page ready. Included in (application.Login)

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,25 +3,74 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.StageStyle;
 
+/**
+*
+* @author Sammy Diesel
+*/
 
-public class Main extends Application {
+public class Main extends Application 
+{
+	//define your offsets here
+    private double xOffset = 0;
+    private double yOffset = 0;
+    
+    
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+	public void start(Stage primaryStage) throws Exception
+	{
+		try 
+		{
+			Parent root = FXMLLoader.load(getClass().getResource("Login/Login.fxml"));
+			
+			//we gonna remove the borderless thingie.  
+	        primaryStage.initStyle(StageStyle.DECORATED);
+	        primaryStage.setMaximized(false);
+
+	        //grab your root here
+	        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+	            @Override
+	            public void handle(MouseEvent event) {
+	                xOffset = event.getSceneX();
+	                yOffset = event.getSceneY();
+	            }
+	        });
+
+	        //sorry about that - Windows defender issue.
+	        //move around here
+	        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+	            @Override
+	            public void handle(MouseEvent event) {
+	                primaryStage.setX(event.getScreenX() - xOffset);
+	                primaryStage.setY(event.getScreenY() - yOffset);
+	            }
+	        });
+			
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} 
+		
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
+	/**
+     * @param args the command line arguments
+     */
+	
+	public static void main(String[] args) 
+	{
 		launch(args);
 	}
 }
+
