90a8370ff3f7dced1618aeb9a6d0cc05a6f811c0
Om Agarwal <omagarwal86@gmail.com>
2021-04-10 23:06:16 +0530

    Login Linked with signin button but not showing the inventory page.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,25 +3,40 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.StageStyle;
 
+public class Main extends Application 
+{
 
-public class Main extends Application {
+    
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	public void start(Stage primaryStage) throws Exception
+	{
+		try 
+		{
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+			
+			//we gonna remove the borderless thingie.  
+	        primaryStage.initStyle(StageStyle.DECORATED);
+			Scene scene = new Scene(root,900,716);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} 
+		
+		catch(Exception e) 
+		{
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		launch(args);
 	}
 }
+
