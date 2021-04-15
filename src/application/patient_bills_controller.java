d90f3bdb6daf75b3c03dcfa36052bc89801f5711
Om Agarwal <omagarwal86@gmail.com>
2021-04-15 19:59:51 +0530

    Patient Java Linking Finished.Few Linkings of admin is done. Need to change the schemas in dbs as well as the FXML files.

--- src/application/patient_bills_controller.java
+++ src/application/patient_bills_controller.java
@@ -2,8 +2,15 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import Utils.ConnectionUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +18,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class patient_bills_controller implements Initializable {
@@ -29,6 +39,79 @@ public class patient_bills_controller implements Initializable {
 	@FXML
 	private Button settings;
 	
+	@FXML
+	private TableView<Bills> Bill_Report;
+	
+	@FXML
+	private TableColumn<Bills,String>Bill_No;
+	
+	@FXML
+	private TableColumn<Bills,String>Room_Charge;
+	
+	@FXML
+	private TableColumn<Bills,String>Treatment_Charge;
+	
+	@FXML
+	private TableColumn<Bills,String>Medicine_Charge;
+	
+	@FXML
+	private TableColumn<Bills,String>Net_Amount;
+	
+	public String userId,Phone_No;
+	
+	public void showInfo(String name,String Phone) {
+		userId=name;
+		Phone_No=Phone;
+	}
+	
+	String query = null;
+    Connection connection = null ;
+    PreparedStatement preparedStatement = null ;
+    ResultSet resultSet = null ;
+    Bills bills=null;
+	
+    ObservableList<Bills>bills_list= FXCollections.observableArrayList();
+    
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+    	loadDate();
+	}
+    
+    private void loadDate() {
+    	connection=ConnectionUtil.ConDB();
+    	 try {
+    		 bills_list.clear();
+             
+             query = "SELECT * FROM bill";
+             preparedStatement = connection.prepareStatement(query);
+             resultSet = preparedStatement.executeQuery();
+             
+             while (resultSet.next()){
+            	 boolean add=bills_list.add(new  Bills(
+                         resultSet.getInt("Bill_No"),
+                         resultSet.getInt("Room_Charge"),
+                         resultSet.getInt("Treatment_Charge"),
+                         resultSet.getInt("Medicine_Charge"),
+                         resultSet.getInt("Net_Amount")));
+            	 Bill_Report.setItems(bills_list);
+                 
+             }
+             
+             
+         } catch (SQLException ex) {
+        	 System.out.println("Error");
+//             Logger.getLogger(med_report_controller.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    	 Bill_No.setCellValueFactory(new PropertyValueFactory<>("Bill_No"));
+    	 Room_Charge.setCellValueFactory(new PropertyValueFactory<>("Room_Charge"));
+    	 Treatment_Charge.setCellValueFactory(new PropertyValueFactory<>("Treatment_Charge"));
+    	 Medicine_Charge.setCellValueFactory(new PropertyValueFactory<>("Medicine_Charge"));
+    	 Net_Amount.setCellValueFactory(new PropertyValueFactory<>("Net_Amount"));
+    }
+
+    
+    
 	public void home(ActionEvent event) {
 		try {
             //add you loading or delays - ;-)
@@ -113,10 +196,6 @@ public class patient_bills_controller implements Initializable {
         }
 	}
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 }
--- src/application/visit_details_controller.java
+++ src/application/visit_details_controller.java
@@ -2,8 +2,15 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import Utils.ConnectionUtil;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,6 +18,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class visit_details_controller implements Initializable {
@@ -29,6 +39,161 @@ public class visit_details_controller implements Initializable {
 	
 	@FXML
 	private Button settings;
+	
+	
+	// In Patient
+	@FXML
+	private TableView<Patient_In> Pateint_In_Details;
+	
+	@FXML
+	private TableColumn<Patient_In,String>pid;
+	
+	@FXML
+	private TableColumn<Patient_In,String>in_date;
+	
+	@FXML
+	private TableColumn<Patient_In,String>out_date;
+	
+	@FXML
+	private TableColumn<Patient_In,String>room_no;
+	
+	@FXML
+	private TableColumn<Patient_In,String>report_no;
+	
+	@FXML
+	private TableColumn<Patient_In,String>bill_no;
+	
+	@FXML
+	private TableColumn<Patient_In,String>treatment;
+	
+	@FXML
+	private TableColumn<Patient_In,String>presc;
+	
+	@FXML
+	private TableColumn<Patient_In,String>adv_pay;
+	
+	
+	// Out Patient
+	@FXML
+	private TableView<Patient_Out> Pateint_Out_Details;
+
+	@FXML
+	private TableColumn<Patient_In,String>pid_op;
+	
+	@FXML
+	private TableColumn<Patient_In,String>in_date_op;
+	
+	@FXML
+	private TableColumn<Patient_In,String>report_no_op;
+	
+	@FXML
+	private TableColumn<Patient_In,String>bill_no_op;
+	
+	@FXML
+	private TableColumn<Patient_In,String>treatment_op;
+	
+	@FXML
+	private TableColumn<Patient_In,String>presc_op;
+	
+	
+	
+	public String userId,Phone_No;	
+	public void showInfo(String name,String Phone) {
+		userId=name;
+		Phone_No=Phone;
+	}
+	
+	String query = null;
+    Connection connection = null ;
+    PreparedStatement preparedStatement = null ;
+    ResultSet resultSet = null ;
+    Patient_In pin=null;
+    Patient_Out pout=null;
+    
+    ObservableList<Patient_In>Patient_In_list= FXCollections.observableArrayList();
+    
+    ObservableList<Patient_Out>Patient_Out_list= FXCollections.observableArrayList();
+    
+    @Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+    	loadDate_In();
+    	loadDate_Out();
+	}
+    
+    private void loadDate_In() {
+    	connection=ConnectionUtil.ConDB();
+    	 try {
+    		 Patient_In_list.clear();
+             
+             query = "SELECT * FROM in_pat";
+             preparedStatement = connection.prepareStatement(query);
+             resultSet = preparedStatement.executeQuery();
+             
+             while (resultSet.next()){
+            	 boolean add=Patient_In_list.add(new  Patient_In(
+                         resultSet.getInt("pid"),
+                         resultSet.getDate("in_date"),
+                         resultSet.getDate("out_date"),
+                         resultSet.getInt("room_no"),
+                         resultSet.getInt("report_no"),
+                         resultSet.getInt("bill_no"),
+                         resultSet.getString("treatment"),
+                         resultSet.getString("presc"),
+                         resultSet.getInt("adv_pay")));
+            	 Pateint_In_Details.setItems(Patient_In_list);
+                 
+             }
+             
+             
+         } catch (SQLException ex) {
+        	 System.out.println("Error");
+//             Logger.getLogger(med_report_controller.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    	 pid.setCellValueFactory(new PropertyValueFactory<>("pid"));
+    	 in_date.setCellValueFactory(new PropertyValueFactory<>("in_date"));
+    	 out_date.setCellValueFactory(new PropertyValueFactory<>("out_date"));
+    	 room_no.setCellValueFactory(new PropertyValueFactory<>("room_no"));
+    	 report_no.setCellValueFactory(new PropertyValueFactory<>("report_no"));
+    	 bill_no.setCellValueFactory(new PropertyValueFactory<>("bill_no"));
+    	 treatment.setCellValueFactory(new PropertyValueFactory<>("treatment"));
+    	 presc.setCellValueFactory(new PropertyValueFactory<>("presc"));
+    	 adv_pay.setCellValueFactory(new PropertyValueFactory<>("adv_pay"));
+    }
+    
+    private void loadDate_Out() {
+    	connection=ConnectionUtil.ConDB();
+    	 try {
+    		 Patient_Out_list.clear();
+             
+             query = "SELECT * FROM out_pat";
+             preparedStatement = connection.prepareStatement(query);
+             resultSet = preparedStatement.executeQuery();
+             
+             while (resultSet.next()){
+            	 boolean add=Patient_Out_list.add(new  Patient_Out(
+                         resultSet.getInt("pid_op"),
+                         resultSet.getDate("in_date_op"),
+                         resultSet.getInt("report_no_op"),
+                         resultSet.getInt("bill_no_op"),
+                         resultSet.getString("treatment_op"),
+                         resultSet.getString("presc_op")));
+            	 Pateint_Out_Details.setItems(Patient_Out_list);
+                 
+             }
+             
+             
+         } catch (SQLException ex) {
+        	 System.out.println("Error");
+//             Logger.getLogger(med_report_controller.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    	 pid_op.setCellValueFactory(new PropertyValueFactory<>("pid_op"));
+    	 in_date_op.setCellValueFactory(new PropertyValueFactory<>("in_date_op"));
+    	 report_no_op.setCellValueFactory(new PropertyValueFactory<>("report_no_op"));
+    	 bill_no_op.setCellValueFactory(new PropertyValueFactory<>("bill_no_op"));
+    	 treatment_op.setCellValueFactory(new PropertyValueFactory<>("treatment_op"));
+    	 presc_op.setCellValueFactory(new PropertyValueFactory<>("presc_op"));
+    }
 	
 	public void home(ActionEvent event) {
 		try {
@@ -115,10 +280,5 @@ public class visit_details_controller implements Initializable {
 	}
 	
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
